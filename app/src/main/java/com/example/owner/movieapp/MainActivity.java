235df91f15fb01f0f3c71d1b82d8062f1f249f99
mrdofu <intense_smurf@live.com>
2016-04-18 23:15:19 -0400

    created main activity layout

--- app/src/main/java/com/example/owner/movieapp/MainActivity.java
+++ app/src/main/java/com/example/owner/movieapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.owner.movieapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridview_movies);
+        gridView.setAdapter();
     }
 }
