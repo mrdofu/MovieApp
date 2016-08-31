1b420ac32538e9a1261139f9b3f4adf434093ed6
Dom Fung <intense_smurf@live.com>
2016-08-31 12:37:45 -0400

    fixed movie for parcelable transfer

--- app/src/main/java/com/example/owner/movieapp/Movie.java
+++ app/src/main/java/com/example/owner/movieapp/Movie.java
@@ -7,42 +7,45 @@ import android.os.Parcelable;
  * Created by Owner on 2016-04-21.
  */
 public class Movie implements Parcelable {
-    private String posterPath;
     private String title;
+    private String posterPath;
     private String synopsis;
-    private float rating;
+    private double rating;
     private String releaseDate;
-
-    public String getReleaseDate() {
-        return releaseDate;
-    }
-
-    public String getSynopsis() {
-        return synopsis;
-    }
 
     public String getTitle() {
         return title;
     }
-
     public String getPosterPath() {
         return posterPath;
     }
-
-    public float getRating() {
+    public String getSynopsis() {
+        return synopsis;
+    }
+    public double getRating() {
         return rating;
+    }
+    public String getReleaseDate() {
+        return releaseDate;
     }
 
     public Movie(String title, String posterPath, String synopsis, double rating, String releaseDate) {
         this.title = title;
         this.posterPath = posterPath;
         this.synopsis = synopsis;
-        this.rating = (float) rating;
+        this.rating = rating;
         this.releaseDate = releaseDate;
     }
 
+    /*
+     * The below is for parcelable
+     */
     private Movie(Parcel in) {
-
+        title = in.readString();
+        posterPath = in.readString();
+        synopsis = in.readString();
+        rating = in.readDouble();
+        releaseDate = in.readString();
     }
 
     public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
@@ -61,5 +64,10 @@ public class Movie implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel out, int flags) {
+        out.writeString(title);
+        out.writeString(posterPath);
+        out.writeString(synopsis);
+        out.writeDouble(rating);
+        out.writeString(releaseDate);
     }
 }
